8c8bde717e6fac8fc85db2bbecdb0ab5625a90d3
seosmall <61647681+seosmall@users.noreply.github.com>
2020-09-07 13:04:49 +0700

    Update morsoonline.js

--- morsoonline.js
+++ morsoonline.js
@@ -19,8 +19,11 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*3)+1);var xxx=null;if(x=="1"){xxx="financing.html"}
-if(x=="2"){xxx="origination.html"}
-if(x=="3"){xxx="estate.html"}a_to_ve[i].href="http://idsly.morsodifame.com/1999/01/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="lover.html"}
+if(x=="3"){xxx="estate.html"}
+if(x=="4"){xxx="tips.html"}
+if(x=="5"){xxx="habit.html"}
+if(x=="6"){xxx="yourself.html"}a_to_ve[i].href="https://data.morsodifame.com/1999/01/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
