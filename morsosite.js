1bb2289cce90545beb61f0f64a9a7f06e641d1ce
seosmall <61647681+seosmall@users.noreply.github.com>
2022-10-20 22:01:14 +0700

    Update morsosite.js

--- morsosite.js
+++ morsosite.js
@@ -18,12 +18,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*6)+1);var xxx=null;if(x=="1"){xxx="retirement.html"}
-if(x=="2"){xxx="environmental.html"}
-if(x=="3"){xxx="refinance.html"}
-if(x=="4"){xxx="fun.html"}
-if(x=="5"){xxx="love.html"}
-if(x=="6"){xxx="unique.html"}a_to_ve[i].href="https://data.morsodifame.com/1999/01/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*7)+1);var xxx=null;if(x=="1"){xxx="ideas.html"}
+if(x=="2"){xxx="opportunities.html"}
+if(x=="3"){xxx="careers.html"}
+if(x=="4"){xxx="healthcare.html"}
+if(x=="5"){xxx="business.html"}
+if(x=="6"){xxx="management.html"}
+if(x=="7"){xxx="health.html"}a_to_ve[i].href="http://www.mogie.us/1999/01/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
